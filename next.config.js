f3bbd092cce4b290b9e2844354d70b1a5a03e48f
tk1114632 <smh91@163.com>
2025-07-23 12:39:04 +0800

    4

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const packageJson = require('./package.json')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	distDir: 'build',
 	output: 'standalone',
 	images: {
 		remotePatterns: [
